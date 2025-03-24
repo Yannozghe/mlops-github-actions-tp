4f32f834a6d69d019d08fcb3e4736272170021a4
Yannozghe <139814441+Yannozghe@users.noreply.github.com>
2025-03-24 14:10:32 +0100

    To change the python version

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 def predict_sentiment(text):
-	if not text:
-		return "neutral"
-	if "happy" in text.lower() or "good" in text.lower():
-		return "positive"
-	if "sad" in text.lower() or "bad" in text.lower():
-		return "negative"
-	return "neutral"
+    if not text:
+        return "neutral"
+    text = text.lower()  # Transformation en minuscules pour éviter des erreurs de casse
+    if "happy" in text or "good" in text:
+        return "positif"  # Changement du retour de "positive" à "positif"
+    if "sad" in text or "bad" in text:
+        return "negative"
+    return"neutral"
